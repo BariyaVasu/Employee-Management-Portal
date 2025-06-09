adbd0f78c895ee79539e9ec4508aedfcc0f55583
Vasu-0846 <bariyavasu46@gmail.com>
2025-06-09 10:44:06 +0530

    nothing

--- main.js
+++ main.js
@@ -1,46 +1,46 @@
-    let employees = [];
-    let editIndex = null;
-    const form = document.getElementById('employee-form');
-    const nameInput = document.getElementById('name');
-    const emailInput = document.getElementById('email');
-    const roleInput = document.getElementById('role');
-    const tableBody = document.querySelector('#employee-table body');
+let employees = [];
+let editIndex = null;
+const form = document.getElementById('employee-form');
+const nameInput = document.getElementById('name');
+const emailInput = document.getElementById('email');
+const roleInput = document.getElementById('role');
+const tableBody = document.querySelector('#employee-table body');
 
-    //Even listener for adding or updating employee
+//Even listener for adding or updating employee
 
-    form.addEventListener('submit', function(e) {
-        e.preventDefault();
-        const name = nameInput.value.trim();
-        const email = emailInput.value.trim();
-        const role = roleInput.value.trim();
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
+    const name = nameInput.value.trim();
+    const email = emailInput.value.trim();
+    const role = roleInput.value.trim();
 
 
-        //case 1 ->  for any value missing
-        if (!name ||!email ||!role) return;
-        
-        const employeeData = { name,email,role};
+    //case 1 ->  for any value missing
+    if (!name || !email || !role) return;
 
-        if(editIndex===null){
-            employees.push(employeeData);
-            
-        }else{
-            employees[editIndex] = employeeData;
-            editIndex = null;
-            form.querySelector("button").innerHTML="Add Employee";
+    const employeeData = { name, email, role };
 
-        }
-        form.reset();
-        renderTable();
+    if (editIndex === null) {
+        employees.push(employeeData);
 
-    })
-    //table render
+    } else {
+        employees[editIndex] = employeeData;
+        editIndex = null;
+        form.querySelector("button").innerHTML = "Add Employee";
 
-    function renderTable(){
-        const tableBody = document.querySelector("tbody");
-        tableBody.innerHTML = " ";
-        employees.forEach((employee, index) => {
-            const row = document.createElement('tr');
-            row.innerHTML = `
+    }
+    form.reset();
+    renderTable();
+
+})
+//table render
+
+function renderTable() {
+    const tableBody = document.querySelector("tbody");
+    tableBody.innerHTML = " ";
+    employees.forEach((employee, index) => {
+        const row = document.createElement('tr');
+        row.innerHTML = `
                 <td>${employee.name}</td>
                 <td>${employee.email}</td>
                 <td>${employee.role}</td>
@@ -49,51 +49,58 @@
                     <button class="delete-btn" onclick="deleteEmployee(${index})">Delete</button>
                 </td>
             `;
-            tableBody.appendChild(row);
-            // saveData();
-        });
+        tableBody.appendChild(row);
+        getData();
+    });
+}
+
+
+//Edit Employee
+
+function editEmployee(index) {
+    const employee = employees[index];
+    nameInput.value = employee.name;
+    emailInput.value = employee.email;
+    roleInput.value = employee.role;
+    editIndex = index;
+    form.querySelector("button").innerHTML = "Update Employee";
+    getData();
+
+
+}
+
+
+//Delete Employee
+
+function deleteEmployee(index) {
+    if (confirm("Are you sure to delete this employee?")) {
     }
+    employees.splice(index, 1);
+    renderTable();
+    getData();
+}
+
+//use filter method
+
+// function deleteEmployee(index){
+//     if(confirm("Are you sure to delete this employee?")){
+//         employees = employees.filter((employee, i) => i!== index);
+//         renderTable();
+//     }
+// }
 
 
-    //Edit Employee
-
-    function editEmployee(index){
-        const employee = employees[index];
-        nameInput.value = employee.name;
-        emailInput.value = employee.email;
-        roleInput.value = employee.role;
-        editIndex = index;
-        form.querySelector("button").innerHTML="Update Employee";
-
-
-    }
-
-
-    //Delete Employee
-
-    function deleteEmployee(index){
-        if(confirm("Are you sure to delete this employee?")){
-        }
-        employees.splice(index,1);
-        renderTable();
-    }
-
-    //use filter method
-
-    // function deleteEmployee(index){
-    //     if(confirm("Are you sure to delete this employee?")){
-    //         employees = employees.filter((employee, i) => i!== index);
-    //         renderTable();
-    //     }
-    // }
-
-    window.addEventListener("load", function(){
-        if(localStorage.getItem('employees')){
+function getData() {
+    window.addEventListener("load", function () {
+        if (localStorage.getItem('employees')) {
             employees = JSON.parse(localStorage.getItem('employees'));
             renderTable();
         }
     });
+}
 
-    window.addEventListener("beforeunload", function(){
-        localStorage.setItem('employees',JSON.stringify(employees));
-    });
+window.addEventListener("beforeunload", function () {
+    localStorage.setItem('employees', JSON.stringify(employees));
+});
+
+getData();
